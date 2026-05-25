240312fd7699032deb52da90b5784671fcb268cc
Thimeo-dev <thimeosousa02@gmail.com>
2026-05-25 20:30:41 +0200

    luxembourg

--- pays/data.js
+++ pays/data.js
@@ -538,7 +538,7 @@ export const countriesData = {
     },
     "luxembourg": {
         image: "https://flagcdn.com/w160/lu.png",
-        landscape: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/LX-VCF%40HKG_%2820190501121917%29.jpg/1920px-LX-VCF%40HKG_%2820190501121917%29.jpg",
+        landscape: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Luxembourg_City_Chambre_des_d%C3%A9put%C3%A9s_Nov_2009.jpg/1280px-Luxembourg_City_Chambre_des_d%C3%A9put%C3%A9s_Nov_2009.jpg",
         coords: [49.815, 6.129],
         fr: {
             name: "Luxembourg",

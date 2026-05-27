5ae2f646648885a512d0582f911ee80f468b2fc4
Thimeo-dev <thimeosousa02@gmail.com>
2026-05-27 21:10:09 +0200

    nav

--- main.js
+++ main.js
@@ -118,8 +118,8 @@ const authLinks = user
                 </li>
             `
             : `
-                <li><a href="${pathPrefix}video.html" data-key="nav_video"></a></li>
                 <li><a href="${pathPrefix}index.html" data-key="nav_home"></a></li>
+                <li><a href="${pathPrefix}video.html" data-key="nav_video"></a></li>
                 <li><a href="${pathPrefix}auth.html" class="login-btn" id="auth-btn" data-key="nav_login"></a></li>
             `;
 

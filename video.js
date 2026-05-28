942a7c17160c8386447cce486ec181e952ed9d32
Thimeo-dev <thimeosousa02@gmail.com>
2026-05-28 09:12:40 +0200

    video youtube

--- video.js
+++ video.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const videoId = this.getAttribute('data-video-id');
             
             if (videoId) {
-                // On crée l'iframe dynamique avec autoplay activé
+                // 1. On injecte l'iframe dynamique avec l'autoplay activé
                 this.innerHTML = `
                     <iframe 
                         src="https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0" 
@@ -18,6 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
                         allowfullscreen>
                     </iframe>
                 `;
+
+                // 2. On sélectionne l'iframe qui vient d'être créée
+                const iframe = this.querySelector('iframe');
+
+                // 3. On demande au navigateur de passer l'iframe en plein écran immédiatement
+                if (iframe.requestFullscreen) {
+                    iframe.requestFullscreen();
+                } else if (iframe.webkitRequestFullscreen) { /* Pour Safari */
+                    iframe.webkitRequestFullscreen();
+                } else if (iframe.msRequestFullscreen) { /* Pour Internet Explorer / Edge */
+                    iframe.msRequestFullscreen();
+                }
             }
         });
     });

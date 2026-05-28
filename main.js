c447ab63e5f3246c9ac32cc71340f9ab6f320f7a
Thimeo-dev <thimeosousa02@gmail.com>
2026-05-28 12:16:27 +0200

    main

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app); 
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
